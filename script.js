8003dd9d08fe679a46d950fff6e98e70e194b883
Hiep Dinh <hiep.dinh@okiela.com>
2020-04-08 16:58:23 +0700

    Add directive v-bind:title to show title in VueJS

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'VueJS - HelloWorld!!!'
+    message: 'VueJS - HelloWorld!!!',
+    hover_title: 'Current time is ' + new Date().toLocaleTimeString()
   }
 })
